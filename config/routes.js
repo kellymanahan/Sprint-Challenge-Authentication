dc984413e34d8c25280f92310b387350e194fc0b
kammagik <kammagik@yahoo.com>
2019-02-17 01:04:38 -0800

    implemented login

--- config/routes.js
+++ config/routes.js
@@ -25,7 +25,21 @@ function register(req, res) {
 }
 
 function login(req, res) {
-  // implement user login
+const creds = req.body;
+db('users')
+.where({username:creds.username})
+.first()
+.then(user=>{
+  if(user && bcrypt.compareSync(creds.password, user.password)){
+    const token = generateToken(user);
+    res.status(200).json({message: 'Welcome'})
+  } else {
+    res.status(401).json({err:'incorrect'})
+  }
+})
+.catch(err=> {
+  res.json(err)
+})
 }
 
 function getJokes(req, res) {
